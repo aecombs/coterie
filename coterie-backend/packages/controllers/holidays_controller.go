bbd0dab05d7bdc7fa314e9112777c80b78ddee08
Alicia Combs <alicia.combs.92@gmail.com>
2020-07-10 21:01:07 -0700

    Added model funcs and controller actions

--- coterie-backend/packages/controllers/holidays_controller.go
+++ coterie-backend/packages/controllers/holidays_controller.go
@@ -1,2 +1,110 @@
 package controllers
 
+import (
+	"coterie/packages/models"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/qkgo/yin"
+)
+
+//Index
+func GetHolidays(holidayTable *models.HolidayTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, _ := yin.Event(w, r)
+
+		holidays, err := holidayTable.HolidaysLister()
+		if err != nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		res.SendJSON(holidays)
+	}
+}
+
+//Show
+func GetHoliday(holidayTable *models.HolidayTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, _ := yin.Event(w, r)
+		holidayID := chi.URLParam(r, "holidayID")
+
+		holiday, err := holidayTable.HolidayGetter(holidayID)
+		if err != nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		res.SendJSON(holiday)
+	}
+}
+
+//Create
+func AddHoliday(holidayTable *models.HolidayTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, req := yin.Event(w, r)
+		body := map[string]string{}
+		req.BindBody(&body)
+
+		orgID, _ := strconv.Atoi(body["organization_id"])
+		holiday := models.Holiday{
+			Text:           body["text"],
+			Date:           body["date"],
+			OrganizationID: orgID,
+			CreatedAt:      time.Now().String(),
+			UpdatedAt:      time.Now().String(),
+		}
+
+		result, err := holidayTable.HolidayAdder(holiday)
+		if err != nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		res.SendJSON(result)
+	}
+}
+
+//Update
+func UpdateHoliday(holidayTable *models.HolidayTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, req := yin.Event(w, r)
+		holidayID := chi.URLParam(r, "holidayID")
+		body := map[string]string{}
+		req.BindBody(&body)
+
+		holID, _ := strconv.Atoi(holidayID)
+		holiday := models.Holiday{
+			ID:        holID,
+			Text:      body["text"],
+			Date:      body["date"],
+			UpdatedAt: time.Now().String(),
+		}
+
+		result, err := holidayTable.HolidayUpdater(holiday)
+		if err != nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		res.SendJSON(result)
+	}
+}
+
+//Delete
+func DeleteHoliday(holidayTable *models.HolidayTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, _ := yin.Event(w, r)
+		holidayID := chi.URLParam(r, "holidayID")
+
+		err := holidayTable.HolidayDeleter(holidayID)
+		if err != nil {
+			http.Error(w, http.StatusText(400), 400)
+			return
+		}
+
+		res.SendStatus(200)
+	}
+}
--- coterie-backend/packages/models/holiday.go
+++ coterie-backend/packages/models/holiday.go
@@ -1,15 +1,18 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+)
 
 type Holiday struct {
-	ID             int
-	Name           string
-	Date           string
-	Description    string
-	OrganizationID int
-	CreatedAt      string
-	UpdatedAt      string
+	ID             int    `json:"id,omitempty"`
+	Name           string `json:"name,omitempty"`
+	Date           string `json:"date,omitempty"`
+	Description    string `json:"description,omitempty"`
+	CreatedAt      string `json:"created_at,omitempty"`
+	UpdatedAt      string `json:"updated_at,omitempty"`
+	OrganizationID int    `json:"organization_id,omitempty"`
 }
 
 type HolidayTable struct {
@@ -20,11 +23,11 @@ func NewHolidayTable(db *sql.DB) *HolidayTable {
 	stmt, _ := db.Prepare(`
 		CREATE TABLE IF NOT EXISTS "holiday" (
 			"ID"	INTEGER NOT NULL UNIQUE,
-			"created_at"	DATETIME,
-			"updated_at"	DATETIME,
 			"name"	TEXT,
-			"date"  DATE,
+			"date"  TEXT,
 			"description"  TEXT,
+			"created_at"	TEXT,
+			"updated_at"	TEXT,
 			"organization_id"	INTEGER,
 			FOREIGN KEY("organization_id") REFERENCES "organization"("ID"),
 			PRIMARY KEY("ID" AUTOINCREMENT)
@@ -32,7 +35,134 @@ func NewHolidayTable(db *sql.DB) *HolidayTable {
 	`)
 
 	stmt.Exec()
+
+	defer stmt.Close()
+
 	return &HolidayTable{
 		DB: db,
 	}
+}
+
+//Model.All
+func (holidayTable *HolidayTable) HolidaysLister() ([]Holiday, error) {
+	holidays := []Holiday{}
+	rows, err := holidayTable.DB.Query(`
+		SELECT * FROM holiday
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var id int
+	var name string
+	var date string
+	var description string
+	var createdAt string
+	var updatedAt string
+	var organizationID int
+	for rows.Next() {
+		rows.Scan(&id, &name, &date, &description, &createdAt, &updatedAt, &organizationID)
+		holiday := Holiday{
+			ID:             id,
+			Name:           name,
+			Date:           date,
+			Description:    description,
+			CreatedAt:      createdAt,
+			UpdatedAt:      updatedAt,
+			OrganizationID: organizationID,
+		}
+		holidays = append(holidays, holiday)
+	}
+	return holidays, err
+}
+
+//Model.where(id: "")
+func (holidayTable *HolidayTable) HolidayGetter(holidayID string) (Holiday, error) {
+	var holiday Holiday
+
+	stmt, err := holidayTable.DB.Prepare(`
+		SELECT * FROM holiday WHERE id = ?
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if stmt != nil {
+		var id int
+		var name string
+		var date string
+		var description string
+		var createdAt string
+		var updatedAt string
+		var organizationID int
+
+		err = stmt.QueryRow(holidayID).Scan(&id, &name, &date, &description, &createdAt, &updatedAt, &organizationID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		holiday.ID = id
+		holiday.Name = name
+		holiday.Date = date
+		holiday.Description = description
+		holiday.CreatedAt = createdAt
+		holiday.UpdatedAt = updatedAt
+		holiday.OrganizationID = organizationID
+	}
+	return holiday, err
+}
+
+//Model.create
+func (holidayTable *HolidayTable) HolidayAdder(holiday Holiday) (Holiday, error) {
+	stmt, err := holidayTable.DB.Prepare(`
+		INSERT INTO holiday (name,date,description,created_at,updated_at,organization_id) VALUES (?,?,?,?,?,?)
+	`)
+
+	stmt.Exec(holiday.Name, holiday.Date, holiday.Description, holiday.CreatedAt, holiday.UpdatedAt, holiday.OrganizationID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	return holiday, err
+}
+
+//Model.update
+func (holidayTable *HolidayTable) HolidayUpdater(holiday Holiday) (Holiday, error) {
+	stmt, err := holidayTable.DB.Prepare(`
+	UPDATE holiday SET name = ?, date = ?, description = ?, updated_at = ? WHERE holiday.id = ?
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(holiday.Name, holiday.Date, holiday.Description, holiday.UpdatedAt, holiday.ID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return holiday, err
+}
+
+//Model.delete
+func (holidayTable *HolidayTable) HolidayDeleter(holidayID string) error {
+	stmt, err := holidayTable.DB.Prepare(`
+		DELETE FROM holiday WHERE holiday.id = ?
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(holidayID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return err
 }
