3b299c0e2b249ea9c5d0db4d9524b52004a79f5f
Alicia Combs <alicia.combs.92@gmail.com>
2020-07-10 18:36:32 -0700

    Implemented prelim controller actions

--- coterie-backend/packages/controllers/chapters_controller.go
+++ coterie-backend/packages/controllers/chapters_controller.go
@@ -1,2 +1,110 @@
 package controllers
 
+import (
+	"coterie/packages/models"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/qkgo/yin"
+)
+
+//Index
+func GetChapters(chapterTable *models.ChapterTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, _ := yin.Event(w, r)
+
+		chapters, err := chapterTable.ChaptersLister()
+		if err != nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		res.SendJSON(chapters)
+	}
+}
+
+//Show
+func GetChapter(chapterTable *models.ChapterTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, _ := yin.Event(w, r)
+		chapterID := chi.URLParam(r, "chapterID")
+
+		chapter, err := chapterTable.ChapterGetter(chapterID)
+		if err != nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		res.SendJSON(chapter)
+	}
+}
+
+//Create
+func AddChapter(chapterTable *models.ChapterTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, req := yin.Event(w, r)
+		body := map[string]string{}
+		req.BindBody(&body)
+
+		orgID, _ := strconv.Atoi(body["organization_id"])
+		chapter := models.Chapter{
+			// Text:           body["text"],
+			// Date:           body["date"],
+			// OrganizationID: orgID,
+			// CreatedAt:      time.Now().String(),
+			// UpdatedAt:      time.Now().String(),
+		}
+
+		result, err := chapterTable.ChapterAdder(chapter)
+		if err != nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		res.SendJSON(result)
+	}
+}
+
+//Update
+func UpdateChapter(chapterTable *models.ChapterTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, req := yin.Event(w, r)
+		chapterID := chi.URLParam(r, "chapterID")
+		body := map[string]string{}
+		req.BindBody(&body)
+
+		chapID, _ := strconv.Atoi(chapterID)
+		chapter := models.Chapter{
+			ID: chapID,
+			// Text:      body["text"],
+			// Date:      body["date"],
+			UpdatedAt: time.Now().String(),
+		}
+
+		result, err := chapterTable.ChapterUpdater(chapter)
+		if err != nil {
+			http.Error(w, http.StatusText(404), 404)
+			return
+		}
+
+		res.SendJSON(result)
+	}
+}
+
+//Delete
+func DeleteChapter(chapterTable *models.ChapterTable) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		res, _ := yin.Event(w, r)
+		chapterID := chi.URLParam(r, "chapterID")
+
+		err := chapterTable.ChapterDeleter(chapterID)
+		if err != nil {
+			http.Error(w, http.StatusText(400), 400)
+			return
+		}
+
+		res.SendStatus(200)
+	}
+}
--- coterie-backend/packages/models/chapter.go
+++ coterie-backend/packages/models/chapter.go
@@ -1,15 +1,17 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 type Chapter struct {
-	ID          int
-	Name        string
-	Text        string
-	Position    int
-	ScriptureID int
-	CreatedAt   string
-	UpdatedAt   string
+	ID          int    `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Text        string `json:"text,omitempty"`
+	Position    int    `json:"position,omitempty"`
+	ScriptureID int    `json:"scripture_id,omitempty"`
+	CreatedAt   string `json:"created_at,omitempty"`
+	UpdatedAt   string `json:"updated_at,omitempty"`
 }
 
 type ChapterTable struct {
@@ -20,11 +22,11 @@ func NewChapterTable(db *sql.DB) *ChapterTable {
 	stmt, _ := db.Prepare(`
 		CREATE TABLE IF NOT EXISTS "chapter" (
 			"ID"	INTEGER NOT NULL UNIQUE,
-			"created_at"	DATETIME,
-			"updated_at"	DATETIME,
 			"name"	TEXT,
 			"text"  TEXT,
 			"position"  INTEGER,
+			"created_at"	TEXT,
+			"updated_at"	TEXT,
 			"scripture_id"	INTEGER,
 			FOREIGN KEY("scripture_id") REFERENCES "scripture"("ID"),
 			PRIMARY KEY("ID" AUTOINCREMENT)
@@ -36,3 +38,123 @@ func NewChapterTable(db *sql.DB) *ChapterTable {
 		DB: db,
 	}
 }
+
+// //Model.All
+// func (announcementTable *AnnouncementTable) AnnouncementsLister() ([]Announcement, error) {
+// 	announcements := []Announcement{}
+// 	rows, err := announcementTable.DB.Query(`
+// 		SELECT * FROM announcement
+// 	`)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer rows.Close()
+
+// 	var id int
+// 	var text string
+// 	var date string
+// 	var createdAt string
+// 	var updatedAt string
+// 	var organizationID int
+// 	for rows.Next() {
+// 		rows.Scan(&id, &text, &date, &createdAt, &updatedAt, &organizationID)
+// 		announcement := Announcement{
+// 			ID:             id,
+// 			Text:           text,
+// 			Date:           date,
+// 			CreatedAt:      createdAt,
+// 			UpdatedAt:      updatedAt,
+// 			OrganizationID: organizationID,
+// 		}
+// 		announcements = append(announcements, announcement)
+// 	}
+// 	return announcements, err
+// }
+
+// //Model.where(id: "")
+// func (announcementTable *AnnouncementTable) AnnouncementGetter(announcementID string) (Announcement, error) {
+// 	var announcement Announcement
+
+// 	stmt, err := announcementTable.DB.Prepare(`
+// 		SELECT * FROM announcement WHERE id = ?
+// 	`)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer stmt.Close()
+
+// 	if stmt != nil {
+// 		var id int
+// 		var text string
+// 		var date string
+// 		var createdAt string
+// 		var updatedAt string
+// 		var organizationID int
+
+// 		err = stmt.QueryRow(announcementID).Scan(&id, &text, &date, &createdAt, &updatedAt, &organizationID)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+
+// 		announcement.ID = id
+// 		announcement.Text = text
+// 		announcement.Date = date
+// 		announcement.CreatedAt = createdAt
+// 		announcement.UpdatedAt = updatedAt
+// 		announcement.OrganizationID = organizationID
+// 	}
+// 	return announcement, err
+// }
+
+// //Model.create
+// func (announcementTable *AnnouncementTable) AnnouncementAdder(announcement Announcement) (Announcement, error) {
+// 	stmt, err := announcementTable.DB.Prepare(`
+// 		INSERT INTO announcement (date,text,created_at,updated_at,organization_id) values (?,?,?,?,?)
+// 	`)
+
+// 	stmt.Exec(announcement.Date, announcement.Text, announcement.CreatedAt, announcement.UpdatedAt, announcement.OrganizationID)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer stmt.Close()
+
+// 	return announcement, err
+// }
+
+// //Model.update
+// func (announcementTable *AnnouncementTable) AnnouncementUpdater(announcement Announcement) (Announcement, error) {
+// 	stmt, err := announcementTable.DB.Prepare(`
+// 	UPDATE announcement SET date = ?, text = ?, updated_at = ? WHERE announcement.id = ?
+// 	`)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer stmt.Close()
+
+// 	_, err = stmt.Exec(announcement.Date, announcement.Text, announcement.UpdatedAt, announcement.ID)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return announcement, err
+// }
+
+// //Model.delete
+// func (announcementTable *AnnouncementTable) AnnouncementDeleter(announcementID string) error {
+// 	stmt, err := announcementTable.DB.Prepare(`
+// 		DELETE FROM announcement WHERE announcement.id = ?
+// 	`)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer stmt.Close()
+
+// 	_, err = stmt.Exec(announcementID)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	return err
+// }
