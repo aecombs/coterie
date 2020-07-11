36234ed5dfe9873b1ba43ff104145da2233ef740
Alicia Combs <alicia.combs.92@gmail.com>
2020-07-10 21:13:19 -0700

    Updated schema for member

--- coterie-backend/packages/models/member.go
+++ coterie-backend/packages/models/member.go
@@ -3,15 +3,15 @@ package models
 import "database/sql"
 
 type Member struct {
-	ID             int
-	Name           string
-	Birthdate      string
-	Class          string
-	Email          string
-	FundsRaised    int
-	OrganizationID int
-	CreatedAt      string
-	UpdatedAt      string
+	ID             int    `json:"id,omitempty"`
+	Name           string `json:"name,omitempty"`
+	Birthdate      string `json:"birthdate,omitempty"`
+	Class          string `json:"class,omitempty"`
+	Email          string `json:"email,omitempty"`
+	FundsRaised    int    `json:"funds_raised,omitempty"`
+	CreatedAt      string `json:"created_at,omitempty"`
+	UpdatedAt      string `json:"updated_at,omitempty"`
+	OrganizationID int    `json:"organization_id,omitempty"`
 }
 
 type MemberTable struct {
@@ -22,13 +22,13 @@ func NewMemberTable(db *sql.DB) *MemberTable {
 	stmt, _ := db.Prepare(`
 		CREATE TABLE IF NOT EXISTS "member" (
 			"ID"	INTEGER NOT NULL UNIQUE,
-			"created_at"	DATETIME,
-			"updated_at"	DATETIME,
 			"name"	TEXT,
-			"birthdate"  DATE,
+			"birthdate"  TEXT,
 			"class"  TEXT,
 			"email"  TEXT,
 			"funds_raised"  INTEGER,
+			"created_at"	TEXT,
+			"updated_at"	TEXT,
 			"organization_id"	INTEGER,
 			FOREIGN KEY("organization_id") REFERENCES "organization"("ID"),
 			PRIMARY KEY("ID" AUTOINCREMENT)
@@ -36,6 +36,9 @@ func NewMemberTable(db *sql.DB) *MemberTable {
 	`)
 
 	stmt.Exec()
+
+	defer stmt.Close()
+
 	return &MemberTable{
 		DB: db,
 	}
