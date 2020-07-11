db0612111c73382bdd734d6fdf9becf9753ccd1e
Alicia Combs <alicia.combs.92@gmail.com>
2020-07-10 22:09:03 -0700

    Added scripture model funcs

--- coterie-backend/packages/models/scripture.go
+++ coterie-backend/packages/models/scripture.go
@@ -1,6 +1,9 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+)
 
 type Scripture struct {
 	ID             int    `json:"id,omitempty"`
@@ -34,4 +37,120 @@ func NewScriptureTable(db *sql.DB) *ScriptureTable {
 	return &ScriptureTable{
 		DB: db,
 	}
+}
+
+//Model.All
+func (scriptureTable *ScriptureTable) ScripturesLister() ([]Scripture, error) {
+	scriptures := []Scripture{}
+	rows, err := scriptureTable.DB.Query(`
+		SELECT * FROM scripture
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var id int
+	var name string
+	var createdAt string
+	var updatedAt string
+	var organizationID int
+	for rows.Next() {
+		rows.Scan(&id, &name, &createdAt, &updatedAt, &organizationID)
+		scripture := Scripture{
+			ID:             id,
+			Name:           name,
+			CreatedAt:      createdAt,
+			UpdatedAt:      updatedAt,
+			OrganizationID: organizationID,
+		}
+		scriptures = append(scriptures, scripture)
+	}
+	return scriptures, err
+}
+
+//Model.where(id: "")
+func (scriptureTable *ScriptureTable) ScriptureGetter(scriptureID string) (Scripture, error) {
+	var scripture Scripture
+
+	stmt, err := scriptureTable.DB.Prepare(`
+		SELECT * FROM scripture WHERE id = ?
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if stmt != nil {
+		var id int
+		var name string
+		var createdAt string
+		var updatedAt string
+		var organizationID int
+
+		err = stmt.QueryRow(scriptureID).Scan(&id, &name, &createdAt, &updatedAt, &organizationID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		scripture.ID = id
+		scripture.Name = name
+		scripture.CreatedAt = createdAt
+		scripture.UpdatedAt = updatedAt
+		scripture.OrganizationID = organizationID
+	}
+	return scripture, err
+}
+
+//Model.create
+func (scriptureTable *ScriptureTable) ScriptureAdder(scripture Scripture) (Scripture, error) {
+	stmt, err := scriptureTable.DB.Prepare(`
+		INSERT INTO scripture (name,created_at,updated_at,organization_id) VALUES (?,?,?,?)
+	`)
+
+	stmt.Exec(scripture.Name, scripture.CreatedAt, scripture.UpdatedAt, scripture.OrganizationID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	return scripture, err
+}
+
+//Model.update
+func (scriptureTable *ScriptureTable) ScriptureUpdater(scripture Scripture) (Scripture, error) {
+	stmt, err := scriptureTable.DB.Prepare(`
+	UPDATE scripture SET name = ?, updated_at = ? WHERE scripture.id = ?
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(scripture.Name, scripture.UpdatedAt, scripture.ID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return scripture, err
+}
+
+//Model.delete
+func (scriptureTable *ScriptureTable) ScriptureDeleter(scriptureID string) error {
+	stmt, err := scriptureTable.DB.Prepare(`
+		DELETE FROM scripture WHERE scripture.id = ?
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(scriptureID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return err
 }
