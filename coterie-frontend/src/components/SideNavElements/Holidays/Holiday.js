3a50a9c8b679d116a8dd150d7bd79a5a58af844b
Alicia Combs <alicia.combs.92@gmail.com>
2020-07-19 14:49:42 -0700

    Updated Holiday component

--- coterie-frontend/src/components/SideNavElements/Holidays/Holiday.js
+++ coterie-frontend/src/components/SideNavElements/Holidays/Holiday.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import HolidayForm from './HolidayForm';
 
 const Holiday = (props) => {
+  const [updateMode, setUpdateMode] = useState(false);
+
+  const formatDate = (dateStr) => {
+    const formattedDate = new Date(dateStr).toUTCString();
+    return formattedDate.substring(0,17)
+  }
+
+  const updateHoliday = (holObj) => {
+    props.updateHolidayCallback(holObj)
+  }
+
+  const setUpdate = () => {
+    setUpdateMode(!updateMode);
+  }
+
+
   return (
-    <div className="list-group-item card">
-      <div className="card-body">
-        <p className="open-sans card-title">{props.name}</p>
-        <p className="open-sans card-text">{props.date}</p>
-      </div>
-      <div className="card-body">
-        <p className="open-sans card-text hidden-until-hover">{props.description}</p>
-      </div>
-    </div>
+    <section>
+      <button onClick={setUpdate} className={ updateMode ? "hidden" : "btn list-group-item list-group-item-action"}>
+        <div className="card-body row justify-content-between">
+          <div>
+            <p className="card-title font-weight-bolder text-left">{props.name}</p>
+            <p className="open-sans card-text text-left">{props.description}</p>
+          </div>
+          <div>
+            <p className="open-sans card-text text-right">{formatDate(props.date)}</p>
+          </div>
+        </div>
+      </button>
+      <HolidayForm 
+        id={props.id}
+        name={props.name}
+        date={props.date}
+        description={props.description}
+        orgID={props.orgID}
+        visibility={updateMode}
+        addHolidayCallback={updateHoliday}
+        onSubmitCallback={setUpdate}
+        />
+    </section>
   )
 }
 
